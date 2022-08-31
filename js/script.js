c5b6e7886528b54460c367dc7a1e994c2c107303
Wadzim <wadzim@MacBook-Air-Wadzim.local>
2022-08-31 15:26:33 +0300

    feat: finish xmlhttp form sending

--- js/script.js
+++ js/script.js
@@ -221,6 +221,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // forms
 
+  // const forms = document.querySelectorAll('form');
+
+  // const message = {
+  //   loading: 'loading',
+  //   success: 'thank you! we will contact you soon',
+  //   failure: 'something went wrong'
+  // };
+
+  // forms.forEach(form => {
+  //   postData(form);
+  // });
+
+  // function postData(form) {
+  //   form.addEventListener('submit', (e) => {
+  //     e.preventDefault();
+
+  //     const statusMessage = document.createElement('div');
+  //     statusMessage.classList.add('status');
+  //     statusMessage.textContent = message.loading;
+  //     form.append(statusMessage);
+
+  //     const request = new XMLHttpRequest();
+  //     request.open('POST', 'server.php');
+
+  //     // request.setRequestHeader('Content-type', 'multipart/form-data')
+  //     const formData = new FormData(form);
+
+  //     request.send(formData);
+
+  //     request.addEventListener('load', () => {
+  //       if (request.status === 200) {
+  //         console.log(request.response);
+  //         statusMessage.textContent = message.success;
+  //         form.reset()
+  //         setTimeout(() => {
+  //           statusMessage.remove();
+  //         }, 2000)
+  //       } else {
+  //         statusMessage.textContent = message.failure;
+  //       }
+  //     });
+
+  //   });
+
+  // }
+
+  // forms json
   const forms = document.querySelectorAll('form');
 
   const message = {
@@ -245,15 +292,26 @@ document.addEventListener('DOMContentLoaded', () => {
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
 
-      request.setRequestHeader('Content-type', 'multipart/form-data')
+      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
-      request.send(formData);
+      const object = {};
+      formData.forEach(function(value, key) {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
 
       request.addEventListener('load', () => {
         if (request.status === 200) {
           console.log(request.response);
           statusMessage.textContent = message.success;
+          form.reset()
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000)
         } else {
           statusMessage.textContent = message.failure;
         }
--- server.php
+++ server.php
@@ -1,3 +1,9 @@
+<!-- without json -->
+<!-- <?php
+echo var_dump($_POST);
+?> -->
+<!-- with json -->
 <?php
+$_POST = json_decode(file_get_contents("php://input"), true);
 echo var_dump($_POST);
 ?>
