19145e978c0e0280f3276e988a569cf0a9596461
Wadzim <wadzim.dev@gmail.com>
2022-09-06 13:35:07 +0300

    refactor: get request&menu cards

--- js/script.js
+++ js/script.js
@@ -187,37 +187,25 @@ document.addEventListener('DOMContentLoaded', () => {
     `;
       this.parent.append(div);
     }
-
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    20,
-    '.menu .container',
-  ).render();
+  const getResource = async (url) => {
+    const result = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    25,
-    '.menu .container',
-    'menu__item'
-  ).render();
+    if (!result.ok) {
+      throw new Error(`couldnt fetch ${url}, stats: ${result.status}`);
+    }
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    15,
-    '.menu .container',
-    'menu__item'
-  ).render();
+    return await result.json();
+  };
+
+  getResource('http://localhost:3000/menu')
+    .then(data => {
+      data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
+
 
 
   // forms json
